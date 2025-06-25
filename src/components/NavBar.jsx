917d23dcc889954c3af45e1fad9977093863bd54
Rubo <lucasrubo1@gmail.com>
2025-06-25 00:39:02 -0300

    feat: add main application structure with components for About, Resume, Portfolio, Blog, and Contact pages
    feat: implement sidebar and navbar for navigation
    
    feat: create modal functionality for testimonials in About section
    
    feat: add filtering functionality for portfolio projects
    
    feat: implement contact form with validation
    
    style: add global CSS reset and basic styles
    
    build: configure Vite for React application

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,38 @@
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li className="navbar-item">
+          <button className="navbar-link  active" data-nav-link>
+            About
+          </button>
+        </li>
+
+        <li className="navbar-item">
+          <button className="navbar-link" data-nav-link>
+            Resume
+          </button>
+        </li>
+
+        <li className="navbar-item">
+          <button className="navbar-link" data-nav-link>
+            Portfolio
+          </button>
+        </li>
+
+        <li className="navbar-item">
+          <button className="navbar-link" data-nav-link>
+            Blog
+          </button>
+        </li>
+
+        <li className="navbar-item">
+          <button className="navbar-link" data-nav-link>
+            Contact
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+export default NavBar;
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,106 @@
+function SideBar() {
+  return (
+    <aside className="sidebar" data-sidebar>
+      <div className="sidebar-info">
+        <figure className="avatar-box">
+          <img src="./assets/images/my-avatar.png" alt="Rubo" width="80" />
+        </figure>
+
+        <div className="info-content">
+          <h1 className="name" title="Rubo">
+            Lucas Gabriel Rubo
+          </h1>
+
+          <p className="title">Web developer</p>
+        </div>
+
+        <button className="info_more-btn" data-sidebar-btn>
+          <span>Show Contacts</span>
+
+          <ion-icon name="chevron-down"></ion-icon>
+        </button>
+      </div>
+
+      <div className="sidebar-info_more">
+        <div className="separator"></div>
+
+        <ul className="contacts-list">
+          <li className="contact-item">
+            <div className="icon-box">
+              <ion-icon name="mail-outline"></ion-icon>
+            </div>
+
+            <div className="contact-info">
+              <p className="contact-title">Email</p>
+
+              <a href="mailto:lucasrubo1@gmail.com" className="contact-link">
+                lucasrubo1@gmail.com
+              </a>
+            </div>
+          </li>
+
+          <li className="contact-item">
+            <div className="icon-box">
+              <ion-icon name="phone-portrait-outline"></ion-icon>
+            </div>
+
+            <div className="contact-info">
+              <p className="contact-title">Phone</p>
+
+              <a href="tel:+5519994019804" className="contact-link">
+                +55 (19) 9940-19804
+              </a>
+            </div>
+          </li>
+
+          <li className="contact-item">
+            <div className="icon-box">
+              <ion-icon name="calendar-outline"></ion-icon>
+            </div>
+
+            <div className="contact-info">
+              <p className="contact-title">Birthday</p>
+
+              <time dateTime="1982-06-23">July 12, 2000</time>
+            </div>
+          </li>
+
+          <li className="contact-item">
+            <div className="icon-box">
+              <ion-icon name="location-outline"></ion-icon>
+            </div>
+
+            <div className="contact-info">
+              <p className="contact-title">Location</p>
+
+              <address>Valinhos, São Paulo, Brazil</address>
+            </div>
+          </li>
+        </ul>
+
+        <div className="separator"></div>
+
+        <ul className="social-list">
+          <li className="social-item">
+            <a href="#" className="social-link">
+              <ion-icon name="logo-facebook"></ion-icon>
+            </a>
+          </li>
+
+          <li className="social-item">
+            <a href="#" className="social-link">
+              <ion-icon name="logo-twitter"></ion-icon>
+            </a>
+          </li>
+
+          <li className="social-item">
+            <a href="#" className="social-link">
+              <ion-icon name="logo-instagram"></ion-icon>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  );
+}
+export default SideBar;
