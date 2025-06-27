7927c089272637bfb965e63d922bce2b2da776a6
Rubo <lucasrubo1@gmail.com>
2025-06-27 00:29:32 -0300

    feat: finalizado portfolio incial
    pronto para v1

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,15 +1,60 @@
-import IonIcon from '@reacticons/ionicons';
+import profile from "../data/profile";
+import IonIcon from "@reacticons/ionicons";
+import { useRef, useState, useEffect } from "react";
+
 function Contact() {
+  const formRef = useRef<HTMLFormElement>(null);
+  const [formState, setFormState] = useState({
+    fullname: "",
+    email: "",
+    message: "",
+  });
+
+  const [isValid, setIsValid] = useState(false);
+
+  const isEmailValid = (email: string) => {
+    if (!email) return false;
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  };
+
+  useEffect(() => {
+    const { fullname, email, message } = formState;
+    const valid =
+      fullname.trim().length > 0 &&
+      email.trim().length > 0 &&
+      message.trim().length > 0 &&
+      isEmailValid(email);
+
+    setIsValid(valid);
+  }, [formState]); // Reavalia sempre que o formState mudar
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const { fullname, email, message } = formState;
+    const phone = profile.phone.replace(/\D/g, "");
+    const text = encodeURIComponent(
+      `Name: ${fullname}\nEmail: ${email}\nMessage: ${message}`
+    );
+    const url = `https://wa.me/${phone}?text=${text}`;
+    window.open(url, "_blank");
+  };
+
   return (
     <article className="contact" data-page="contact">
       <header>
-        <h2 className="h2 article-title">Contact</h2>
+        <h2 className="h2 article-title">{profile.contact.title}</h2>
       </header>
 
       <section className="mapbox" data-mapbox>
         <figure>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58768.43300337679!2d-47.01880025!3d-22.986032299999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8cdaee7763fcf%3A0x382e43ff2cf3dd00!2sValinhos%2C%20SP%2C%2013274-465!5e0!3m2!1spt-BR!2sbr!4v1750898025689!5m2!1spt-BR!2sb"
+            src={profile.contact.location}
             width="400"
             height="300"
             loading="lazy"
@@ -18,9 +63,9 @@ function Contact() {
       </section>
 
       <section className="contact-form">
-        <h3 className="h3 form-title">Contact Form</h3>
+        <h3 className="h3 form-title">{profile.contact.form}</h3>
 
-        <form action="#" className="form" data-form>
+        <form ref={formRef} className="form" data-form onSubmit={handleSubmit}>
           <div className="input-wrapper">
             <input
               type="text"
@@ -28,16 +73,21 @@ function Contact() {
               className="form-input"
               placeholder="Full name"
               required
-              data-form-input
+              value={formState.fullname}
+              onChange={handleChange}
             />
-
             <input
               type="email"
               name="email"
-              className="form-input"
+              className={`form-input${
+                formState.email && !isEmailValid(formState.email)
+                  ? " invalid"
+                  : ""
+              }`}
               placeholder="Email address"
               required
-              data-form-input
+              value={formState.email}
+              onChange={handleChange}
             />
           </div>
 
@@ -46,16 +96,18 @@ function Contact() {
             className="form-input"
             placeholder="Your Message"
             required
-            data-form-input
+            value={formState.message}
+            onChange={handleChange}
           ></textarea>
 
-          <button className="form-btn" type="submit" disabled data-form-btn>
-            <IonIcon name="paper-plane"></IonIcon>
-            <span>Send Message</span>
+          <button className="form-btn" type="submit" disabled={!isValid}>
+            <IonIcon name="paper-plane" />
+            <span>{profile.contact.buttonSend}</span>
           </button>
         </form>
       </section>
     </article>
   );
 }
+
 export default Contact;
